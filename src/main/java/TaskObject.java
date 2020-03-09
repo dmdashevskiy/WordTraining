c968fef93958bbff8a94819ebb1438af00205ab8
ikarikarikar <makaroning@gmail.com>
2020-03-09 23:14:31 +0500

    Tack saveable to database.

--- src/main/java/TaskObject.java
+++ src/main/java/TaskObject.java
@@ -31,11 +31,54 @@ public class TaskObject {
 
 	private void saveObjectInDatabase() {
 		
+		PreparedStatement insertObject = null;
+		Connection connectionDatabse = null;
 		
-//		Class.forName ("org.h2.Driver");
-//		Connection connectionDatabse = DriverManager.getConnection ("jdbc:h2:D:\\Eclipse workspace\\WordTraining\\src\\main\\resources/TackBase", "sa","123654");
-//		connectionDatabse.createStatement();
+		String insertString =   "insert into TASK  SET" + 
+								"QUESTION = ?," + 
+								"ANSWER0 = ?," + 
+								"ANSWER1 = ?," + 
+								"ANSWER2 = ?," + 
+								"REPEAT_NUMBER = ?," + 
+								"LAST_REPEAT = ?," + 
+								"NEXT_REPEAT = ?;";	
 		
+		try {
+			Class.forName ("org.h2.Driver");
+			connectionDatabse = DriverManager.getConnection (this.getClass().getResource("TañkBase.mv.db").getPath(), "sa","123654");
+			insertObject = connectionDatabse.prepareStatement(insertString);
+			insertObject.setString(1, this.question);
+			insertObject.setString(2, this.answer0);
+			insertObject.setString(3, this.answer1);
+			insertObject.setString(4, this.answer2);
+			insertObject.setInt(5, REPEAT_NUMBER);
+			insertObject.setDate(5, new java.sql.Date(0));;
+			insertObject.setDate(6, new java.sql.Date(this.nextRepeat.getTimeInMillis()));	
+			insertObject.executeUpdate();			
+
+		} catch (ClassNotFoundException e) {			
+			e.printStackTrace();			
+		} catch (SQLException e) {	       
+			e.printStackTrace();			
+		}
+		finally {
+			if (insertObject != null) {
+				try {
+					insertObject.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}			
+			if (connectionDatabse != null) {
+				try {
+					connectionDatabse.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
+			}
+			
+		}		
 		
 	}
 
