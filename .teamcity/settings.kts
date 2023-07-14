786594dc872181c8b0385928926467056b2d1ba3
ikar <ikar@TeamCity>
2023-07-14 18:04:26 +0500

    TeamCity change in 'WordTraining' project: runners of 'TestBuildConfiguration' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -54,6 +54,7 @@ object TestBuildConfiguration : BuildType({
 
             conditions {
                 equals("Environment", "Stage")
+                equals("SirenaNotificationEnable", "true")
             }
             goals = "clean test"
             coverageEngine = idea {
